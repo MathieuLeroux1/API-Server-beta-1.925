6d6c35c48297624710db8430c0a09199eab79559
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-10-24 11:38:38 -0400

    update verification non nécessaire

--- cachedRequestsManager.js
+++ cachedRequestsManager.js
@@ -13,12 +13,7 @@ export default class CachedRequestsManager {
         console.log(BgWhite + FgBlue, "[Periodic repositories data caches cleaning process started...]");
     }
 
-    static add(url, content, ETag = "", HttpContext) {
-        if (!HttpContext.isCacheable) {
-            console.log(BgWhite + FgBlue, `[Requête non cachable pour l'URL : ${url}]`);
-            return;
-        }
-    
+    static add(url, content, ETag = "") {
         if (!global.cachedRequestsCleanerStarted) {
             global.cachedRequestsCleanerStarted = true;
             CachedRequestsManager.startCachedRequestsCleaner();
